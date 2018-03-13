70de6edec5248317af9f747088a1824c8ae8db72
Don Kelly <karfai@gmail.com>
2018-03-14 03:34:16 +0400

    Changes the service name in the image builder script
    Change-Id: I76f26f276b4a47158e8790a6b5722c57ee6c1246

--- build_images.sh
+++ build_images.sh
@@ -5,7 +5,7 @@ fi
 
 VERSION=`cat .version`
 
-docker build -t "xalgorithms/xadf-respond-service:latest-$ENVIRONMENT" -t "xalgorithms/xadf-respond-service:$VERSION-$ENVIRONMENT" -f "Dockerfile.$ENVIRONMENT" .
+docker build -t "xalgorithms/xadf-events-service:latest-$ENVIRONMENT" -t "xalgorithms/xadf-events-service:$VERSION-$ENVIRONMENT" -f "Dockerfile.$ENVIRONMENT" .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push "xalgorithms/xadf-respond-service:latest-$ENVIRONMENT"
-docker push "xalgorithms/xadf-respond-service:$VERSION-$ENVIRONMENT"
+docker push "xalgorithms/xadf-events-service:latest-$ENVIRONMENT"
+docker push "xalgorithms/xadf-events-service:$VERSION-$ENVIRONMENT"
